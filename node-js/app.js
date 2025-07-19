c00cbb2548201ec3cd56b6d050be9b926db37b92
Ajeesh S A <153414250+Ajeesh-S-A@users.noreply.github.com>
2025-07-19 21:34:13 +0530

    Update app.js

--- node-js/app.js
+++ node-js/app.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from Node.js CI/CD App!');
+  res.send('Hello from Node.js CI/CD App! 1');
 });
 
 app.get('/health', (req, res) => {
